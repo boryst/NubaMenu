37eb65d4a3eb6a3d32d29dbd65f01fa85cbdf2cb
JIu4epo <jiu4epo@gmail.com>
2017-05-19 21:52:15 -0700

    app/build committed

--- app/build/generated/source/r/debug/com/facebook/R.java
+++ app/build/generated/source/r/debug/com/facebook/R.java
@@ -76,15 +76,15 @@ public final class R {
         public static final int com_facebook_tooltip_blue_bottomnub = 0x7f020066;
         public static final int com_facebook_tooltip_blue_topnub = 0x7f020067;
         public static final int com_facebook_tooltip_blue_xout = 0x7f020068;
-        public static final int messenger_bubble_large_blue = 0x7f020074;
-        public static final int messenger_bubble_large_white = 0x7f020075;
-        public static final int messenger_bubble_small_blue = 0x7f020076;
-        public static final int messenger_bubble_small_white = 0x7f020077;
-        public static final int messenger_button_blue_bg_round = 0x7f020078;
-        public static final int messenger_button_blue_bg_selector = 0x7f020079;
-        public static final int messenger_button_send_round_shadow = 0x7f02007a;
-        public static final int messenger_button_white_bg_round = 0x7f02007b;
-        public static final int messenger_button_white_bg_selector = 0x7f02007c;
+        public static final int messenger_bubble_large_blue = 0x7f020075;
+        public static final int messenger_bubble_large_white = 0x7f020076;
+        public static final int messenger_bubble_small_blue = 0x7f020077;
+        public static final int messenger_bubble_small_white = 0x7f020078;
+        public static final int messenger_button_blue_bg_round = 0x7f020079;
+        public static final int messenger_button_blue_bg_selector = 0x7f02007a;
+        public static final int messenger_button_send_round_shadow = 0x7f02007b;
+        public static final int messenger_button_white_bg_round = 0x7f02007c;
+        public static final int messenger_button_white_bg_selector = 0x7f02007d;
     }
     public static final class id {
         public static final int automatic = 0x7f0d0048;
